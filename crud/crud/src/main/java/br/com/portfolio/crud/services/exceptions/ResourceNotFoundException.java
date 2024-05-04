d0a7406fe0efb54a985a296806a47284ee6fc323
Mpsousag <156169858+Mpsousag@users.noreply.github.com>
2024-05-04 15:48:37 -0300

    Add files via upload

---
+++ crud/crud/src/main/java/br/com/portfolio/crud/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,12 @@
+package br.com.portfolio.crud.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException{
+
+	
+	private static final long serialVersionUID = 1L;
+
+	public ResourceNotFoundException(Object id) {
+		super("Não Localizamos um usuário com a id"+id);
+		}
+	
+	}
